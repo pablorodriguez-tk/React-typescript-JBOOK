3959f39690efdd60ccb696cb645665ee5082b373
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-27 22:46:06 -0300

    add logic to fetch packages

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+            namespace: 'a',
+          };
+        }
+        return { path: `https://unpkg.com/${args.path}`, namespace: 'a' };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import React from 'react';
+              console.log(React);
+            `,
+          };
+        }
+        const { data, request } = await axios.get(args.path);
+
+        return {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+      });
+    },
+  };
+};
