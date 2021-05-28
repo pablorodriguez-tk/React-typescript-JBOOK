e576aa3981d898799e47b528facdb18c54f0d31c
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-28 19:44:47 -0300

    Add Load for CSS files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -8,27 +8,47 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-        // Check to see if we have already fetch this file
-        // and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
-
-        // if it is , return inmediately
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const constents = `
+const style = document.createElement('style');
+style.innerText = '${escaped}';
+document.head.appendChild(style);
+`;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: constents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
