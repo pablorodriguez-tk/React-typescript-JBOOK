0d6d5347883696af201341898608efba6ea3d8e5
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-30 01:58:51 -0300

    Add redux reducer with immer

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -18,7 +18,7 @@ export interface DeleteCellAction {
 
 export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
-  payload: { id: string; type: CellTypes };
+  payload: { id: string | null; type: CellTypes };
 }
 
 export interface UpdateCellAction {
